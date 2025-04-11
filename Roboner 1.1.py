e8807f5828ac2ea3ef9f611e7f1d64fa9aebc5be
Liorrr2 <backupliorhe@gmail.com>
2025-04-11 11:55:36 +0300

    Roboner 1.1.py

--- Roboner 1.1.py
+++ Roboner 1.1.py
@@ -1,40 +1,106 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.iodevices import I2CDevice
-from pybricks.parameters import Port
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Color, Stop, Direction
+from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from ev3muxdevices import MuxTouchSensor, MuxColorSensor, MuxInfraredSensor, MuxGyroSensor, MuxUltrasonicSensor
 
-# Подключение к SMUX, который на S2
-smux = I2CDevice(Port.S2, 0x10)
 
-# Адрес ультразвукового датчика по I2C
-ULTRASONIC_ADDR = 0x02
-ultra = I2CDevice(Port.S2, ULTRASONIC_ADDR)
+ev3 = EV3Brick()
+#Movement
+left_motor = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=32, axle_track=185)
 
-# Битовые маски для портов A (0), B (1), C (2)
-mux_ports = {
-    "C1": 0x01,  # порт A (бит 0)
-    "C2": 0x02,  # порт B (бит 1)
-    "C3": 0x04,  # порт C (бит 2)
-}
+#Sensors (port S3 available)
+gyro = GyroSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
+us_front = MuxUltrasonicSensor(4,1)
+us_right = MuxUltrasonicSensor(4,2)
+us_left = MuxUltrasonicSensor(4,3)
+gyro.reset_angle(0)
 
-def read_ultrasonic(mux_mask):
-    # Включить только один порт
-    smux.write(bytes([0x70, mux_mask]))
-    wait(100)  # Ждём стабилизации
+#General Variables
+StartingPoint=None
+DogFound = False
+DogPos = 0
+RoomCounter = 0
+WallDistance=250
 
-    try:
-        ultra.write(b'\x42')           # Регистр с расстоянием
-        data = ultra.read(1)           # Чтение 1 байта
-        return data[0]                 # Возвращаем расстояние
-    except:
-        return None  # Если что-то пошло не так
+def isWall(sensor):
+    return sensor.distance() <=WallDistance
 
-while True:
-    for name, mask in mux_ports.items():
-        dist = read_ultrasonic(mask)
-        if dist is not None:
-            ev3.screen.print(name, dist)
+def LocateRoomEntrance():
+    return color_sensor.color == Color.GREEN
+
+def room_counter():
+    global RoomCounter
+    RoomCounter += 1
+    robot.turn(180)
+    wait(500)
+    robot.straight(100)
+
+
+def get_start_pos():
+    # Если стена справа значит находимся на А
+    if isWall(us_right):
+        # Если к тому же слева препятствие то нашли собаку 
+        if isWall(us_left):
+            DogFound = True
+            DogPos = 1
+    
+    # Если стены справа нет значит развернемся
+    elif not isWall(us_right):
+        # Если к тому же перед роботом препятствие то смотрим на собаку
+        if isWall(us_front):
+            DogFound = True
+            DogPos = 1
+        robot.turn(90)
+        gyro.reset_angle(0)
+
+    Main()
+
+def Main():
+    TURN_ANGLE = 45
+    # Главный алгоритм 
+    #Operating priority: Right > Left > Front > 180Degrees rotation
+    while True:
+        if not(isWall(us_right)):
+            while gyro.angle() <= 90:
+                robot.drive(500, TURN_ANGLE)
+            robot.stop()
+            robot.straight(100)
+            gyro.reset_angle(0)
+            
+        elif not(isWall(us_left)):
+            while gyro.angle() >= -90:
+                robot.drive(500, -TURN_ANGLE)
+            robot.stop()
+            robot.straight(100)
+            gyro.reset_angle(0)
+        
+        elif not(isWall(us_front)):
+            robot.straight(100)
+        
         else:
-            ev3.screen.print(1)
-    wait(1000)  
+            stabilization()
+
+def stabilization():
+    ANGLE = 5
+
+    while isWall(us_right):
+
+        if us_right.distance() <= 200:
+            robot.drive(500, -ANGLE)
+        elif us_left.distance() <= 200:
+            robot.drive(500, ANGLE)
+        else:
+           robot.drive(500, 0)
+
+def ReturnToStartingPoint():
+    # Возвращение на стартовую точку
+    pass
+
+get_start_pos()
